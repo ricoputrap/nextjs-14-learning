bc71ed369c5b4fa380f142fa973860e225e8ce1d
Rico Putra Pradana <pradanaricoputra@gmail.com>
2023-11-08 21:53:34 +0700

    ch-9: show only revenue chart skeleton using Suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,9 +4,10 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices"
 import { lusitana } from "@/app/ui/fonts"
 import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "@/app/lib/data"
 import { Revenue } from "@/app/lib/definitions"
+import { Suspense } from "react"
+import { RevenueChartSkeleton } from "@/app/ui/skeletons"
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
 
   const {
@@ -34,7 +35,9 @@ export default async function Page() {
       </div>
 
       <div className='mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8'>
-        <RevenueChart revenue={revenue}  />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
